8f108a64d5b165812ea3144f88fda82ce7cd4c60
Barreto <v.barreto.and@gmail.com>
2024-09-06 23:23:56 -0300

    initial commit

---
+++ src/main/java/com/e3gsix/fiap/tech_challenge_5_credentials/TechChallenge5CredentialsApplication.java
@@ -0,0 +1,13 @@
+package com.e3gsix.fiap.tech_challenge_5_credentials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechChallenge5CredentialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechChallenge5CredentialsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/e3gsix/fiap/tech_challenge_5_credentials/TechChallenge5CredentialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e3gsix.fiap.tech_challenge_5_credentials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechChallenge5CredentialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
