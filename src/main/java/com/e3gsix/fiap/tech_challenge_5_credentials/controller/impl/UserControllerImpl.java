09e18c6b28f789db361197a27d3bc693f4511e6e
Barreto <v.barreto.and@gmail.com>
2024-09-10 00:34:56 -0300

    add controllers documentation

--- src/main/java/com/e3gsix/fiap/tech_challenge_5_credentials/controller/impl/UserControllerImpl.java
+++ src/main/java/com/e3gsix/fiap/tech_challenge_5_credentials/controller/impl/UserControllerImpl.java
@@ -4,14 +4,19 @@ import com.e3gsix.fiap.tech_challenge_5_credentials.controller.UserController;
 import com.e3gsix.fiap.tech_challenge_5_credentials.model.dto.request.UserUpdateRequest;
 import com.e3gsix.fiap.tech_challenge_5_credentials.model.dto.response.UserResponse;
 import com.e3gsix.fiap.tech_challenge_5_credentials.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
 import static com.e3gsix.fiap.tech_challenge_5_credentials.controller.impl.UserControllerImpl.URL_USERS;
 
+@Tag(name = "Usuários", description = "Gerenciamento de usuários.")
 @RestController
 @RequestMapping(URL_USERS)
 public class UserControllerImpl implements UserController {
@@ -26,6 +31,14 @@ public class UserControllerImpl implements UserController {
         this.userService = userService;
     }
 
+    @Operation(
+            summary = "Buscar usuário pelo ID",
+            description = "Este recurso realiza a busca de um usuário na base de dados através do ID informado.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Usuário encontrado com sucesso."),
+                    @ApiResponse(responseCode = "404", description = "Usuário com ID informado não foi encontrado.")
+            }
+    )
     @Override
     @GetMapping(URL_USERS_FIND_BY_ID)
     public ResponseEntity<UserResponse> findById(@PathVariable UUID id) {
@@ -34,6 +47,16 @@ public class UserControllerImpl implements UserController {
         return ResponseEntity.ok(userFound);
     }
 
+    @Operation(
+            summary = "Atualizar dados do usuário",
+            description = "O endpoint permite que ADMINISTRADORES atualizem informações pertinentes aos usuários.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Usuário encontrado com sucesso."),
+                    @ApiResponse(responseCode = "401", description = "Token não foi informado ou está inválido."),
+                    @ApiResponse(responseCode = "403", description = "Requisitante não tem permissões de administrador."),
+                    @ApiResponse(responseCode = "404", description = "Usuário com ID informado não foi encontrado.")
+            }
+    )
     @Override
     @PutMapping(URL_UPDATE)
     public ResponseEntity<UserResponse> update(@PathVariable UUID id, @RequestBody UserUpdateRequest request) {
