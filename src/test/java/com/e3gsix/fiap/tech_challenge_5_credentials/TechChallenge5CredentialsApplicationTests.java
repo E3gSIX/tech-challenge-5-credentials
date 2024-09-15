142ab2c1d3fbbaf2bf7adbb1b4879cfab27e2f7e
Barreto <v.barreto.and@gmail.com>
2024-09-14 21:42:29 -0300

    add h2 as database for testing

--- src/test/java/com/e3gsix/fiap/tech_challenge_5_credentials/TechChallenge5CredentialsApplicationTests.java
+++ src/test/java/com/e3gsix/fiap/tech_challenge_5_credentials/TechChallenge5CredentialsApplicationTests.java
@@ -2,8 +2,10 @@ package com.e3gsix.fiap.tech_challenge_5_credentials;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TechChallenge5CredentialsApplicationTests {
 
 	@Test
